30ed69b1fb40f33f05772894e3d000aae233ac41
Trevor Vaughn <TrevorJVaughn@gmail.com>
2017-06-14 23:43:02 -1000

    Added hidebehinds, basic hiding behaviors, test scene

---
+++ Assets/Scripts/FindUnseenSpot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindUnseenSpot : MonoBehaviour {
+
+    Transform player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 target = new Vector3(player.position.x, gameObject.transform.position.y, player.position.z);
+        transform.LookAt(target);
+	}
+}
---
+++ Assets/Scripts/HideBehind.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HideBehind : MonoBehaviour {
+
+    Transform playerTransform;
+    NavMeshAgent agent;
+    public GameObject unseenHelperPivot, unseenTarget;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        agent.SetDestination(unseenTarget.transform.position);
+
+        gameObject.transform.LookAt(playerTransform.position);
+	}
+}
